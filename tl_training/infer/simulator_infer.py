4298dbd0350240b27263971b77dfddfe7a64b45a
Alan Steremberg <alan.steremberg@gmail.com>
2017-09-20 08:27:32 -0700

    dump json

--- tl_training/infer/simulator_infer.py
+++ tl_training/infer/simulator_infer.py
@@ -7,6 +7,8 @@ import tarfile
 import tensorflow as tf
 import zipfile
 import json
+import math
+
 
 from collections import defaultdict
 from io import StringIO
@@ -60,9 +62,16 @@ with detection_graph.as_default():
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
+def changelabel(label):
+  if (label=='traffic_light_red'):
+     return 'red'
+  if (label=='traffic_light_green'):
+     return 'green'
+  if (label=='traffic_light_yellow'):
+     return 'yellow'
+  return label
 
-
-def predict(image):
+def predict(image,fname,folder):
       #image = Image.open(image_path)
       # the array based representation of the image will be used later in order to prepare the
       # result image with boxes and labels on it.
@@ -76,6 +85,7 @@ def predict(image):
           feed_dict={image_tensor: image_np_expanded})
       # Visualization of the results of a detection.
       #print((scores))
+      min_score_thresh=0.4
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
           np.squeeze(boxes),
@@ -83,9 +93,38 @@ def predict(image):
           np.squeeze(scores),
           category_index,
           use_normalized_coordinates=True,
-          min_score_thresh=0.2,
+          min_score_thresh=min_score_thresh,
           line_thickness=4)
-      return image_np
+
+      data = {}
+      data['filename']=fname
+      data['folder']=folder
+      (width,height) =  image.size
+      data['image_w_h']= image.size
+      data['objects']=[]
+      scores2=np.squeeze(scores)
+      boxes2=np.squeeze(boxes)
+      classes2=np.squeeze(classes).astype(np.int32)
+  
+      for i in range(boxes2.shape[0]):
+         if scores2 is None or scores2[i] > min_score_thresh:
+                  box = tuple(boxes2[i].tolist())
+                  if classes2[i] in category_index.keys():
+                     class_name = changelabel(category_index[classes2[i]]['name'])
+                  label = {}
+                  label['label']=class_name
+                  #print(box)
+                  x = int(round(box[1]*width))
+                  y = int(round(box[0]*height))
+                  x2 = int(round(box[3]*width))
+                  y2 = int(round(box[2]*height))
+                  label['x_y_w_h']= (x,y,(x2-x),(y2-y))
+                  data['objects'].append(label)
+      jsondata=json.dumps(data)
+      print(jsondata)
+
+
+      return image_np,jsondata
 
 
 
@@ -98,11 +137,14 @@ def processVideo(input_video,output):
   out_clip.write_videofile(output,audio=False)
 
 
-if __name__ == '__main__':
-  path = '../data/simulator_imgs/green'
-  outpath = '../data/simulator_imgs/greenout'
+def processDir(color):
+  path = '../data/simulator_imgs/'+color
+  outpath = '../data/simulator_imgs/'+color+'out'
+  jsonoutpath = '../data/simulator_imgs/'+color+'out/annotations'
   if not os.path.exists(outpath):
     os.makedirs(outpath)
+  if not os.path.exists(jsonoutpath):
+    os.makedirs(jsonoutpath)
   rootDir = path
   for dirName, subdirList, fileList in os.walk(rootDir):
     print('Found directory: %s' % dirName)
@@ -115,9 +157,20 @@ if __name__ == '__main__':
               encoded_jpg = fid.read()
               encoded_jpg_io = io.BytesIO(encoded_jpg)
               image = Image.open(encoded_jpg_io)
-              image_np = predict(image)
-              plt.figure(figsize=IMAGE_SIZE)
-              plt.imshow(image_np)
-              plt.savefig(os.path.join(outpath, '{}'.format(fname)))
-              plt.close()
+              image_np,jsondata = predict(image,fname,color)
+              im = Image.fromarray(image_np)
+              im.save(os.path.join(outpath, '{}'.format(fname)))
+              #plt.figure(figsize=IMAGE_SIZE)
+              #plt.imshow(image_np)
+              #plt.savefig(os.path.join(outpath, '{}'.format(fname)))
+              #plt.close()
+              jsonname = fname.replace('jpg','json')
+              jsonfilename=(os.path.join(jsonoutpath, '{}'.format(jsonname)))
+              with open(jsonfilename, 'w') as outfile:
+                  outfile.write(jsondata)
+                  outfile.close()
 
+if __name__ == '__main__':
+   processDir('green')
+   processDir('yellow')
+   processDir('red')
