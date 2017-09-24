515a608398dbc8bda9d9047aa8c413e64c33c29a
Ed Venator <evenator@swri.org>
2017-09-24 10:25:56 -0400

    Clean up tl_detector for merge
    - PEP8 compliance
    - PEP257 docstrings in module
    - Make ROS topics relative instead of absolute
    - Move rospy node initialization and spin() out of node constructor
    - Remove commented out code
    - Remove unneeded debug statements
    - Add warning when detection doesn't match ground truth

--- ros/src/tl_detector/src/tl_detector/tl_classifier.py
+++ ros/src/tl_detector/src/tl_detector/tl_classifier.py
@@ -1,68 +1,63 @@
-"""
-TODO:
-1. Uncomment, "from styx_msgs.msg import TrafficLight"
-2. Take care of CV image's BGR --to--> RGB (?) format conversion.
-3. Return the "index" corresponding to the color of TL.
-4. Switch between sim and carla models based on some config setting (?). 
-"""
-
-from styx_msgs.msg import TrafficLight
-
+"""Includes a class for classifying traffic lights."""
+from collections import defaultdict
+import cv2
+import io
+from io import StringIO
 import numpy as np
 import os
-import io
+from PIL import Image
+import rospy
+from styx_msgs.msg import TrafficLight
 import sys
 import tensorflow as tf
-import rospy
-from collections import defaultdict
-from io import StringIO
-from PIL import Image
-import cv2
 
-
-PATH_TO_CKPT = rospy.get_param("/tl_inference_graph")
-
-#PATH_TO_CKPT = "../../../../tl_training/train/tl_inferencesimulator/frozen_inference_graph.pb"
-#PATH_TO_CKPT = "./frozen_inference_graph.pb"
 NUM_CLASSES = 3
+
+# For translating from TLClassifier light codes to TrafficLight light codes
+LIGHT_CODES = [TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN]
 
 
 class TLClassifier(object):
-    def __init__(self):
-        self.sess = None
+    """
+    A class to classify traffic lights.
+
+    Uses a TensorFlow classifier to determine the state of a traffic light.
+    """
+
+    def __init__(self, path_to_ckpt, min_score_thresh=0.4):
+        """
+        Constructor.
+
+        path_to_ckpt - Full path to the protobuff definition of the graph.
+        min_score_thresh - Minimum ratio of boxes of one detection in order to accept it.
+        """
+        self.min_score_thresh = min_score_thresh
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
-          self.od_graph_def = tf.GraphDef()
-          with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            self.serialized_graph = fid.read()
-            self.od_graph_def.ParseFromString(self.serialized_graph)
-            tf.import_graph_def(self.od_graph_def, name='')
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
         with self.detection_graph.as_default():
-            self.sess=tf.Session(graph=self.detection_graph)
+            self.sess = tf.Session(graph=self.detection_graph)
             # Definite input and output Tensors for detection_graph
             self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
             # Each box represents a part of the image where a particular object was detected.
-            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
             # Each score represent how level of confidence for each of the objects.
             # Score is shown on the result image, together with the class label.
-            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
-
-        pass
-
-    def changelabel(self,label):
-        if (label=='traffic_light_red'):
-            return 'red'
-        if (label=='traffic_light_green'):
-            return 'green'
-        if (label=='traffic_light_yellow'):
-            return 'yellow'
-        return label
-
+            detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+            self.output_tensors = [detection_boxes,
+                                   detection_scores,
+                                   detection_classes,
+                                   num_detections]
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        """
+        Determine the color of the traffic light in the image.
 
         Args:
             image (cv::Mat): image containing the traffic light
@@ -71,78 +66,65 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        rospy.loginfo("get_classifictation called with image")
         category_index = {1: {'id': 1, 'name': u'traffic_light_red'},
-                  2: {'id': 2, 'name': u'traffic_light_yellow'},
-                  3: {'id': 3, 'name': u'traffic_light_green'}}
-
-
-        light = ['red','yellow','green']
+                          2: {'id': 2, 'name': u'traffic_light_yellow'},
+                          3: {'id': 3, 'name': u'traffic_light_green'}}
 
         # we trained the network with RGB images
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_np = np.array(image)
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image_np, axis=0)
         # Actual detection.
-        (boxes, scores, classes, num) = self.sess.run(
-            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
-            feed_dict={self.image_tensor: image_np_expanded})
+        feed_dict = {self.image_tensor: image_np_expanded}
+        boxes, scores, classes, num = self.sess.run(self.output_tensors, feed_dict=feed_dict)
 
-        # this threshold may want to be 0.2 ??
-        min_score_thresh=0.4
-        width, height, channels =  image.shape
-        #rospy.loginfo("w,h.c: "+str((width,height,channels)))
+        width, height, channels = image.shape
 
-        scores2=np.squeeze(scores)
-        boxes2=np.squeeze(boxes)
-        classes2=np.squeeze(classes).astype(np.int32)
+        scores2 = np.squeeze(scores)
+        boxes2 = np.squeeze(boxes)
+        classes2 = np.squeeze(classes).astype(np.int32)
 
         green_count = 0
         red_count = 0
         yellow_count = 0
         class_name = None
-        #rospy.loginfo("about to look through boxes: "+str((scores2,boxes2)))
 
         for i in range(boxes2.shape[0]):
-            if scores2 is None or scores2[i] > min_score_thresh:
+            if scores2 is None or scores2[i] > self.min_score_thresh:
                 box = tuple(boxes2[i].tolist())
                 if classes2[i] in category_index.keys():
-                    class_name = self.changelabel(category_index[classes2[i]]['name'])
-                #label = {}
-                #label['label']=class_name
+                    class_name = category_index[classes2[i]]['name']
 
                 x = int(round(box[1]*width))
                 y = int(round(box[0]*height))
                 x2 = int(round(box[3]*width))
                 y2 = int(round(box[2]*height))
-                rospy.loginfo("traffic light: "+str((class_name,x,y,x2,y2)))
+                rospy.loginfo("traffic light: " + str((class_name, x, y, x2, y2)))
 
-                if class_name == 'green':
+                if class_name == 'traffic_light_green':
                     green_count += 1
-                elif class_name == 'red':
+                elif class_name == 'traffic_light_red':
                     red_count += 1
-                elif class_name == 'yellow':
+                elif class_name == 'traffic_light_yellow':
                     yellow_count += 1
 
         tl = [red_count, yellow_count, green_count]
-        lightcode = [TrafficLight.RED,TrafficLight.YELLOW,TrafficLight.GREEN]
-        rospy.loginfo("answer: "+str(tl.index(max(tl))))
-        return lightcode[tl.index(max(tl))]
+        max_tl = np.argmax(tl)
+        rospy.loginfo("get_classification answer: %d", max_tl)
+        return LIGHT_CODES[max_tl]
 
-
-tl = TLClassifier()
 
 if __name__ == '__main__':
+    tl = TLClassifier(sys.argv[1])
 
-  # Testing. TODO: Nitin: Make sure that we pass the image in the right (RGB) format.
-  filenames = ['red.jpg','yellow.jpg','green.jpg']
-  for fname in filenames:
-    with open(fname) as data_file:
-      with tf.gfile.GFile(fname, 'rb') as fid:
-        encoded_jpg = fid.read()
-        encoded_jpg_io = io.BytesIO(encoded_jpg)
-        image = Image.open(encoded_jpg_io)
-        light = tl.get_classification(image)
-        print("Light is: ",light)
-
+    # TODO: Nitin: Make sure that we pass the image in the right (RGB) format.
+    filenames = ['red.jpg', 'yellow.jpg', 'green.jpg']
+    for fname in filenames:
+        with open(fname) as data_file:
+            with tf.gfile.GFile(fname, 'rb') as fid:
+                encoded_jpg = fid.read()
+                encoded_jpg_io = io.BytesIO(encoded_jpg)
+                image = Image.open(encoded_jpg_io)
+                light = tl.get_classification(image)
+                print("Light is: ", light)
