c3ec4c46fba25462edfb19a1b77fe3e9d7e9c671
Alan Steremberg <alan.steremberg@gmail.com>
2017-09-26 10:39:43 -0700

    fixed infer for Carla

--- tl_training/infer/tl_infer_Carla.py
+++ tl_training/infer/tl_infer_Carla.py
@@ -20,9 +20,8 @@ from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
 
-PATH_TO_CKPT = "../train/tl_inference3_faster_r-cnn/frozen_inference_graph.pb"
+PATH_TO_CKPT = "../train/tl_inferenceCarla_faster_r-cnn/frozen_inference_graph.pb"
 PATH_TO_LABELS = "../data/output.pbtxt"
-NUM_CLASSES = 3
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -37,10 +36,6 @@ def load_image_into_numpy_array(image):
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
-#label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-#categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-#category_index = label_map_util.create_category_index(categories)
 
 category_index = {1: {'id': 1, 'name': u'traffic_light_green'},
                   2: {'id': 2, 'name': u'traffic_light_red'},
