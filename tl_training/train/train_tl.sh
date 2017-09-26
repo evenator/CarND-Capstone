d8993af34020b156f2a8e3d65e9ae53045a2a648
Alan Steremberg <alan.steremberg@gmail.com>
2017-09-26 10:18:03 -0700

    simplified training shell scripts

--- tl_training/train/train_tl.sh
+++
@@ -1,4 +0,0 @@
-#rm  NITIN_Checkpoint3/*
-#python $OBJECTDETECTION/models/object_detection/train.py --logtostderr --pipeline_config_path=./ssd_inception_v2_coco_udacity_traffic3_NITIN.config --train_dir=./NITIN_Checkpoint3
-rm  NITIN_CheckpointSimulator/*
-python $OBJECTDETECTION/models/object_detection/train.py --logtostderr --pipeline_config_path=./ssd_inception_v2_coco_udacity_simulator3_NITIN.config --train_dir=./NITIN_CheckpointSimulator
