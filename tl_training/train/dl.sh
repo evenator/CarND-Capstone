4a7dc3b917a48d142c2c5c176505fb27da1a42bb
Alan Steremberg <alan.steremberg@gmail.com>
2017-09-26 10:14:29 -0700

    switched model download to faster rcnn

--- tl_training/train/dl.sh
+++ tl_training/train/dl.sh
@@ -1,2 +1,2 @@
-curl -OL http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_11_06_2017.tar.gz
-tar -xzf ssd_inception_v2_coco_11_06_2017.tar.gz
+curl -OL http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
+tar -xzf faster_rcnn_resnet101_coco_11_06_2017.tar.gz
