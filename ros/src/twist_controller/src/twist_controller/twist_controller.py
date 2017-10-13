3e549f9b4f15ee46637f96bd707fb4f0ab670121
Ed Venator <evenator@swri.org>
2017-10-12 20:28:55 -0400

    Various improvements to twist controller
    - Apply a speed ramp constraint to limit acceleration
    - Fix a regression in acceleration calculation
    - Reorder and label calculations in control() to make them
      easier to follow
    - Add debug topics debug_cmd_acc, debug_cmd_vel, and debug_cur_acc for
      plotting
    - Break brake calculation into its own function
    - Make steering calculation function only a function of curvature

--- ros/src/twist_controller/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/src/twist_controller/twist_controller.py
@@ -1,6 +1,7 @@
 """Throttle, brake, and steering control."""
 from math import atan
-from rospy import loginfo
+from rospy import loginfo, Publisher
+from std_msgs.msg import Float32
 from .lowpass import LowPassFilter
 from .pid import PID
 
@@ -22,6 +23,7 @@ class Controller(object):
         Set up gains and initialize internal state; initialize PID controllers.
         """
         self.last_cur_lin_vel = 0
+        self.last_cmd_vel = 0
         self.accel_limit = accel_limit
         self.decel_limit = decel_limit
         self.vehicle_mass = vehicle_mass
@@ -33,6 +35,10 @@ class Controller(object):
         self.accel_filter = LowPassFilter(0.2, 1./rate)
         self.throttle_pid = PID(kp_throttle, ki_throttle, 0.0, 0, 1.)
 
+        self.debug_cmd_a = Publisher('~debug_cmd_acc', Float32, queue_size=2)
+        self.debug_cmd_vel = Publisher('~debug_cmd_vel', Float32, queue_size=2)
+        self.debug_cur_a = Publisher('~debug_cur_acc', Float32, queue_size=2)
+
     def control(self, cmd_lin_vel, cmd_ang_vel, cur_lin_vel, cur_ang_vel, delta_t, dbw_enabled):
         """
         Control update.
@@ -40,29 +46,40 @@ class Controller(object):
         This function should be called at the rate given in the constructor.
         Returns a tuple of (throttle, brake, steering)
         """
+        # Steering
+        if abs(cmd_lin_vel) > 0.1:
+            curvature = cmd_ang_vel / cmd_lin_vel
+        else:
+            curvature = cmd_ang_vel / 0.1
+        steering = self.calculate_steering(curvature)
 
+        # Acceleration limiting (velocity ramp)
+        min_vel = self.last_cmd_vel + self.decel_limit * delta_t
+        max_vel = self.last_cmd_vel + self.accel_limit * delta_t
+        cmd_lin_vel = max(min_vel, min(cmd_lin_vel, max_vel))
+        self.last_cmd_vel = cmd_lin_vel
+
+        # Acceleration Measurement Calculation
         vel_error = cmd_lin_vel - cur_lin_vel
-        raw_acc = cur_lin_vel - self.last_cur_lin_vel
+        raw_acc = (cur_lin_vel - self.last_cur_lin_vel)/delta_t
         cur_acc = self.accel_filter.filt(raw_acc)
         self.last_cur_lin_vel = cur_lin_vel
 
+        # If DBW not enabled, reset and return
         if not dbw_enabled:
             self.reset()
             return (0, 0, 0)
 
-        steering = self.calculate_steering(cmd_ang_vel, cmd_lin_vel)
-
+        # Speed PID to get commanded acceleration
         cmd_acc = self.speed_pid.step(vel_error, delta_t)
 
-        if cmd_acc > self.accel_limit:
-            cmd_acc = self.accel_limit
-        elif cmd_acc < self.decel_limit:
-            cmd_acc = self.decel_limit
+        # Acceleration limiting (commanded acceleration)
+        cmd_acc = max(self.decel_limit, min(cmd_acc, self.accel_limit))
 
-        throttle_error = (cmd_acc - cur_acc)
-
-        brake = -cmd_acc*self.vehicle_mass*self.wheel_radius
+        # Throttle and brake calculations
+        throttle_error = cmd_acc - cur_acc
         throttle = self.throttle_pid.step(throttle_error, delta_t)
+        brake = self.calculate_brake(cmd_acc)
 
         if cmd_acc < 0:
             if brake < self.brake_deadband:
@@ -71,15 +88,21 @@ class Controller(object):
         else:
             brake = 0.
 
+        self.debug_cmd_vel.publish(Float32(data=cmd_lin_vel))
+        self.debug_cmd_a.publish(Float32(data=cmd_acc))
+        self.debug_cur_a.publish(Float32(data=cur_acc))
         return throttle, brake, steering
 
-    def calculate_steering(self, cmd_ang_vel, cmd_lin_vel):
-        # TODO: This threshold can probably be closer to 0
-        if abs(cmd_lin_vel) < 1.0:
-            return 0.0
-        else:
-            # TODO: Remove the magic number here
-            return 0.8*atan(self.wheel_base * cmd_ang_vel / cmd_lin_vel) * self.steer_ratio
+    def calculate_steering(self, curvature):
+        # TODO: Remove this magic number
+        return 0.8*atan(self.wheel_base * curvature) * self.steer_ratio
+
+    def calculate_brake(self, cmd_acc):
+        '''
+        Calculate brake torque to achieve desired deceleration
+        '''
+        force = -cmd_acc * self.vehicle_mass
+        return force * self.wheel_radius
 
     def reset(self):
         self.last_cur_lin_vel = 0
